17957d0e6e180c40a22b53e565a6184d85670719
Egor305 <Egor.chelak@gmail.com>
2013-08-31 03:23:39 +0300

    This should be raw. like literaly

--- raw.php
+++ raw.php
@@ -2,7 +2,7 @@
 if ($_GET['id']) {
     $response = file_get_contents("http://figgycity50.kd.io/ccbin/api.php?type=get&data=json&id=".$_GET['id']);
     $paste_data = json_decode($response, true);
-    echo '<pre style="word-wrap: break-word; white-space: pre-wrap;">'.$paste_data['contents'].'</pre>';
+    echo $paste_data['contents'];
 } else {
     echo 'Error: Paste ID is invalid.';
 }
