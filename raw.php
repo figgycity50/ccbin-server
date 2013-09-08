1c1e2c9366f40fa664f0868b3003713ab1510223
figgycity50 <vm-1.figgycity50.koding.kd.io>
2013-09-08 16:24:48 +0000

    fixed the raw looking minified in browser

--- raw.php
+++ raw.php
@@ -2,6 +2,7 @@
 if ($_GET['id']) {
     $response = file_get_contents("http://figgycity50.kd.io/ccbin/api.php?type=get&data=json&id=".$_GET['id']);
     $paste_data = json_decode($response, true);
+    header('Content-Type: text/plain');
     if($_GET['mode'] == title) echo $paste_data['name'];
     else echo $paste_data['contents'];
 } else {
